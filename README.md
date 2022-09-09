eddd24ea90838ba59bec90638ccf2fa12f4760c1
Janak Mallik <janakmallik20@gmail.com>
2022-09-09 20:46:41 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movie-Ticket-Management-System
+ 
---
+++ src/movieticketmanagementsystem/ConnectMSSQL.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieticketmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class ConnectMSSQL {
+    
+//    public Connection connection;
+//            
+//    public  void connectDB() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            connection = DriverManager.getConnection(
+//                    "jdbc:sqlserver://localhost:1433;databaseName=movie_ticket_v1;ConnectMSSQLselectMethod=cursor", "sa", "123456");
+//
+//            System.out.println("DATABASE NAME IS:" + connection.getMetaData().getDatabaseProductName());
+//
+//            
+//            
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    
+//    public static void main(String[] args) {
+//        ConnectMSSQL cnObj = new ConnectMSSQL();
+//        cnObj.connectDB();
+//    }
+}
+
